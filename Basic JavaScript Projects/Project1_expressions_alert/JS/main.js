b2d036c2f484d54f5afdd367be2a30aad7f7d243
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-06 14:03:44 -0700

    Used var to assign multiple values
    Used Var to assign multiple values to different things

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -11,5 +11,10 @@ window.alert(One);
 document.write("List told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
 
 document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr.Seuss");
+
 var B = "concatenated" + " String";
 document.write(B);
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+
+document.write(Dad);
