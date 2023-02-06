0b92946732264f8b90e83495845a9868261f6eec
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-06 13:59:22 -0700

    Used concantation and \
    Cancatenated a string and used \

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -4,3 +4,10 @@ document.write("Hello, world!");
 
 var A = "This is a string";
 document.write(A);
+
+var B = "1 plus 1 is 21";
+window.alert(B);
+
+document.write("List told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
+
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr.Seuss");
