7f1e0f3dc2826bd4742431fedc174c3fc7339d40
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-06 14:01:49 -0700

    Used Concatenated strings
    Learning more on how to use Concatenated strings

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -5,9 +5,11 @@ document.write("Hello, world!");
 var A = "This is a string";
 document.write(A);
 
-var B = "1 plus 1 is 21";
-window.alert(B);
+var One = "1 plus 1 is 21";
+window.alert(One);
 
 document.write("List told Bart, \"knock it off, Bart! or I'll tell dad!\"<br>\"Eat my shorts!\" Bart responded.");
 
 document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr.Seuss");
+var B = "concatenated" + " String";
+document.write(B);
