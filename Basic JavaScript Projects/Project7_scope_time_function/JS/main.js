2474442a5d7c8b962d0fee3c0ce1572c94a68df7
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-13 11:57:02 -0700

    Auto correct changed my code
    I typed in value and it entered ariavaluemax. So I had to change it back

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -26,6 +26,8 @@ Add_numbers_3();
 Add_numbers_4();
 
 
+
+
 if (10 < 12) {
     document.write("10 is less than 12 duh");
 }
@@ -34,4 +36,18 @@ function get_Date() {
     if (new Date().getHours() < 18) {
         document.getElementById("oompa").innerHTML = "How are you today?";
     }
+}
+
+
+
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
 }
