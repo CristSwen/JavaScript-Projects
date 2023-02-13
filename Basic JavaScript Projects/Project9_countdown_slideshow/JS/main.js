a21e05f70a96ce7277e3e358a4a3270b65002e7e
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-13 16:22:40 -0700

    Fixed two errors
    Moved the script down to the bottom of the body. Removed an extra s with the slideindex

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -32,7 +32,7 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slidesIndex = 1}
+    if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"
