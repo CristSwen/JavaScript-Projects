b001c56a605bbee5617b759a8dc5e33049003505
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-14 13:43:40 -0700

    Added for loop, array and constant function
    Added a For loop and array to practice with new terms. Began writing a constant function

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -7,3 +7,29 @@ function Call_Loop() {
     }
     document.getElementById("Loop").innerHTML = num;
 }
+
+
+var word = "Hello World";
+var length = word.length;
+document.getElementById("Word").innerHTML = length;
+
+
+var instruments = ["Violin", "Viola", "Cello", "Bass", "Trumpet", "Bassoon"];
+var content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < instruments.length; Y++) {
+        content += instruments[Y] + "<br>";
+    }
+    document.getElementById("List_Of_Instruments").innerHTML = content
+}
+
+
+function array_Function() {
+    var Best_Cat = [];
+    Best_Cat[0] = "Tabby";
+    Best_Cat[1] = "Calico";
+    Best_Cat[2] = "Siberian Forest";
+    Best_Cat[3] = "Maine Coon";
+    document.getElementById("Array").innerHTML = "The best cat in world is a " + Best_Cat[3] + ".";
+}
