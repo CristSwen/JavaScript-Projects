ee2b9b3c2dac598e52c86cc1c4af89cea8cca10e
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-02-14 14:24:14 -0700

    Added comments
    Added comments throughout the code to show what each function or block of code does

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,4 +1,4 @@
-function Call_Loop() {
+function Call_Loop() {      //This function is a While loop. While X is less than 10 it will add to num and X.
     var num = "";
     var X = 1;
     while (X < 10) {
@@ -9,12 +9,12 @@ function Call_Loop() {
 }
 
 
-var word = "Hello World";
+var word = "Hello World";           //This block of code shows how to print the length of a var. This will print 11
 var length = word.length;
 document.getElementById("Word").innerHTML = length;
 
 
-var instruments = ["Violin", "Viola", "Cello", "Bass", "Trumpet", "Bassoon"];
+var instruments = ["Violin", "Viola", "Cello", "Bass", "Trumpet", "Bassoon"];       //This code/function shows the array being utilitzed in a for loop. the instruments will print out.
 var content = "";
 var Y;
 function for_Loop() {
@@ -25,7 +25,7 @@ function for_Loop() {
 }
 
 
-function array_Function() {
+function array_Function() {         //This array function shows how to create an empty array, add to it, and call it in a statement
     var Best_Cat = [];
     Best_Cat[0] = "Tabby";
     Best_Cat[1] = "Calico";
@@ -35,7 +35,7 @@ function array_Function() {
 }
 
 
-function constant_function() {
+function constant_function() {              //This constant function is showing how the dictionary is constant, but can be added to and changed. 
     const Musical_Instrument = {type:"guitar", brand:"Fender", color:"Black"};
     Musical_Instrument.color = "blue";
     Musical_Instrument.price = "$900";
@@ -43,13 +43,26 @@ function constant_function() {
 }
 
 
-var L = 22;
+var L = 22;         //This code makes use of the Let statement to show that Let will only change the L within its block scope
 {
     let L = 56;
 }
 document.getElementById("Let").innerHTML = L;
 
-function myFunction(name) {
+
+function myFunction(name) {         //This function makes use of the return statement to return "Hello" with a name that was input
     return "Hello " + name;
 }
 document.getElementById("Return").innerHTML = myFunction("Sakura");
+
+
+let car = {             //Utilizing the let statement and return to concatenate a sentence using those variables
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function() {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
